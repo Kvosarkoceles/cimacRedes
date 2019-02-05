dbd5d4d27effd6711e33dd75676061e1caebd9f4
Kvosarkoceles <kvo.sarkoceles@ciencias.unam.mx>
2019-02-05 02:14:03 -0600

    Edita periodista limpio

---
+++ application/views/app/graficas/graficas.php
@@ -0,0 +1,34 @@
+<!-- pie chart start -->
+
+    <div class="col-lg-12 mt-5">
+        <div class="card">
+            <div id="ampiechart1"></div>
+        </div>
+    </div>
+    <div class="col-lg-6 mt-5">
+        <div class="card">
+            <div id="ampiechart2"></div>
+        </div>
+    </div>
+    <div class="col-lg-6 mt-5">
+        <div class="card">
+            <div id="ampiechart3"></div>
+        </div>
+    </div>
+    <div class="col-lg-6 mt-5">
+        <div class="card">
+            <div id="highpiechart4"></div>
+        </div>
+    </div>
+    <div class="col-lg-6 mt-5">
+        <div class="card">
+            <div id="highpiechart5"></div>
+        </div>
+    </div>
+    <div class="col-lg-6 mt-5">
+        <div class="card">
+            <div id="highpiechart6"></div>
+        </div>
+    </div>
+
+<!-- pie chart end -->
